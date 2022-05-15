57609f00342a334532c0360226ffa2962397792c
polkawer1 <70429254+polkawer1@users.noreply.github.com>
2022-05-15 17:52:34 +0300

    aasddd
    asdd

--- polkabot.py
+++ polkabot.py
@@ -1,6 +1,22 @@
 import discord
-from random import randint
+from discord.utils import get
+a = '~'
+
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged on as {0}!'.format(self.user))
+        await client.change_presence(status = discord.Status.online, activity = discord.Game( 'game'))
+    async def on_message(self, message):
+        print('Message from {0.author}: {0.content}'.format(message))
+        msg = message.content.lower()
 
 
 
-print("hello world")
+
+
+
+
+
+client = MyClient()
+client.run('ODQxNTY1MjkwNjQ2NDcwNjY2.GnGLq9.k019uTchDokz57xkb8ighQwxKfxc2wYq75All4')
